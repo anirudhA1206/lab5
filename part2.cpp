98c8e9398c44d0523a977bdb8cd4413dc5b2aec3
anirudhA1206 <123839709+anirudhA1206@users.noreply.github.com>
2023-04-27 16:24:03 -0400

    Update part2.cpp

--- part2.cpp
+++ part2.cpp
@@ -89,6 +89,10 @@ int main(int argc, char* argv[]) {
 	cl::sycl::accessor temp(temp_buf, handler, cl::sycl::write_only);
 
 	// TODO: Create a parallel_for to implement the first kernel.
+	    handler.parallel_for<class b_x_a_plus_c>(cl::sycl::range<1> { a_h.size() }, [=](cl::sycl::id<1> i) {
+	    temp[i] = b * a_d[i] + c;
+	  });
+      });
 	
       });
 
@@ -99,6 +103,12 @@ int main(int argc, char* argv[]) {
 	cl::sycl::accessor d_d(d_buf, handler, cl::sycl::write_only);
 	
 	// TODO: Create a parallel_for to implement the second kernel.
+	    handler.parallel_for<class clip>(cl::sycl::range<1> { d_h.size() }, [=](cl::sycl::id<1> i) {
+	      if (temp[i] > 127) {d_d[i] = 127;}
+              else if (temp[i] < -128 ) {d_d[i] = -128;}
+              else {d_d[i] = temp[i];}
+	  });
+      });
 	
       });
 
